f68b595a178874e57465722471c92a96fa70532a
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2022-09-29 09:55:51 +0530

    aded .env file & set up routes

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
 
 // express app
 const app = express();
 
+//routes
+app.checkout("/", (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+});
+
 //listen for requests
-app.listen(4000, () => { 
-    console.log('listening for requests on port 4000');
-})
+app.listen(process.env.PORT, () => {
+  console.log("listening for requests on port!");
+});
+
