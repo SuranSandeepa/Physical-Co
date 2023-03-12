29df4e55fc20d457ef442ed07d5682223ad425db
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2023-03-12 17:18:10 +0530

    nodemon

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 //express app
 const app = express();
 
 //listen for requests
 app.listen(4000, () => {
-  console.log("listening on port 4000");
-});
+    console.log('listening on port 4000');
+})
